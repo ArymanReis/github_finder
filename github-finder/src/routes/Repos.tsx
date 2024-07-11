621146e4b6b1a7d1c2535de0af0161dcf55f7039
Aryman Reis <arymanreis@gmail.com>
2024-07-11 12:09:57 -0300

    Finalização

--- github-finder/src/routes/Repos.tsx
+++ github-finder/src/routes/Repos.tsx
@@ -5,8 +5,10 @@ import { useParams } from "react-router-dom";
 
 import BackBtn from "../components/BackBtn";
 import Loader from "../components/Loader";
+import Repo from "../components/Repo";
 
 import classes from './Repos.module.css';
+
 
 
 const Repos = () => {
@@ -28,7 +30,11 @@ const Repos = () => {
 
     setIsLoading(false);
 
-    setRepos(data);
+    let orderedRepos = data.sort((a: RepoProps, b: RepoProps) => b.stargazers_count -a.stargazers_count);
+
+      orderedRepos = orderedRepos.slice(0, 5);
+
+    setRepos(orderedRepos);
 
   };
 
@@ -40,15 +46,15 @@ const Repos = () => {
   if (!repos && isLoading) return <Loader />;
 
   return (
-    <div>
+    <div className={classes.repos}>
       <BackBtn />
       <h2>Explore os repositórios do usuário: {username}</h2>
       {repos && repos.length === 0 && <p>Não há repositórios.</p>}
       {repos && repos.length > 0 && (
-        <div>
+        <div className={classes.repos_container}>
           {repos.map((repo: RepoProps) => (
-            <p>{repo.name}</p>
-          ))};
+            <Repo key={repo.name} {...repo}/>
+          ))}
         </div>
       )}
     </div>
